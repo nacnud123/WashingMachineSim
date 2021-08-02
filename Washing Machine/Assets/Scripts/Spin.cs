e0ea75cb0bbed8863f0db3344dcb2e0ac136355b
DryWall <978duncan2@gmail.com>
2021-08-02 10:22:27 -0400

    init

---
+++ Washing Machine/Assets/Scripts/Spin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float rotateSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+    }
+}
